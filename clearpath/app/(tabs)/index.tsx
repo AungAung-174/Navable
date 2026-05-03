089ae4403583a0e0bd3e4c3a20f7141a1092ca53
Tisya <tomart@oregonstate.edu>
2026-05-02 20:47:40 -0700

    Home page of website
    worked on homepage of our website

--- clearpath/app/(tabs)/index.tsx
+++ clearpath/app/(tabs)/index.tsx
@@ -1,10 +1,5 @@
-import { ScrollView, Text, TouchableOpacity, View } from 'react-native'
-import { globalStyles } from '@/constants/styles'
 
 export default function HomeScreen()
 {
   return
-  (
-    <ScrollView
-  )
 }
